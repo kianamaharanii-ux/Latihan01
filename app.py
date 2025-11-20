5169be81f4ff52e0cbc0d773e936062db4ad1711
Kiana maharani <kianamaharanii@gmail.com>
2025-11-20 14:53:05 +0700

    Memperbaiki file app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
 pages = [
-    st.page(page="pages/page1.py", title="Home", icon="🥨"),
-    st.page(page="pages/page2.py", title="Visualisasi Data", icon="🍕"),
-    st.page(page="pages/page3.py", title="Settings", icon="🧀")
+    st.Page(page="pages/page1.py", title="Home", icon="🥨"),
+    st.Page(page="pages/page2.py", title="Visualisasi Data", icon="🍕"),
+    st.Page(page="pages/page3.py", title="Settings", icon="🧀")
 ]
 
 pg = st.navigation(
